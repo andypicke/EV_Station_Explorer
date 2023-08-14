46e0ab4bb1e4bfa1fd57d4c477479d0dc01db2a2
Andy <andypicke@gmail.com>
2023-08-14 09:51:20 -0600

    create empty app files to start with

--- EV_Station_Explorer/ui.R
+++ EV_Station_Explorer/ui.R
@@ -11,23 +11,19 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 fluidPage(
+  
+  # Application title
+  titlePanel("EV Charging Station Explorer"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    
+  ),
+  
+  # Show a plot of the generated distribution
+  mainPanel(
+    plotOutput("distPlot")
+  )
+  
+) # fluidPage
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
-)
