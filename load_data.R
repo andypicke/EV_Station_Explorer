4842abb802b4aa2e4555f3251b35338ef5396880
Andy <andypicke@gmail.com>
2023-08-18 10:56:14 -0600

    clean up data processing/loading scripts

--- load_data.R
+++ load_data.R
@@ -1,10 +1,13 @@
+#
+# load_data.R 
+#
+# A script to load data for the EV_Station_Explorer Shiny app
+#
+# Data are downloaded/processed in load_process_data.R
+#
+#
+#
 
-#library(dplyr)
-
-data_date <- "2023-08-08"
-fname <- paste0(data_date,'_Elec_Stations_US_All_df')
-
-ev <- readRDS(paste0('./data/',fname))
 
 states_map <- readRDS(file.path(".","data","states_map") )
 
--- load_process_data.R
+++ load_process_data.R
@@ -1,18 +1,61 @@
+#
+# load_process_data.R
+#
+# This script loads/processes/saves data for use in EV_Station_Explorer Shiny app
+#
+#
+#
+#
+
 
 library(dplyr)
 
+
+# Load data for all EV stations previously downloaded from AFDC API
 data_date <- "2023-08-08"
 fname <- paste0(data_date,'_Elec_Stations_US_All_df')
 ev <- readRDS(paste0('./data/',fname))
 
-state_counts <- 
-  ev %>% count(state) %>% 
-  arrange(desc(n)) %>% 
+
+# Code to download US states shapefile using tigris and save
+
+# states_map <- tigris::states() %>% 
+#   janitor::clean_names() %>% 
+#   mutate(statefp = as.numeric(statefp)) %>% 
+#   filter(statefp < 60,
+#          !statefp %in% c(2, 15)) %>% 
+#   rmapshaper::ms_simplify()
+# 
+# saveRDS(states_map, file.path(".","data","states_map") )
+# states_map <- readRDS(file.path(".","data","states_map"))
+
+
+# Count # stations by state
+
+# all stations
+state_counts_total <- 
+  ev %>% 
+  count(state, name = "n_total") %>% 
   as_tibble()
 
-states_map <- tigris::states() %>% 
-  janitor::clean_names() %>% 
-  mutate(statefp = as.numeric(statefp)) %>% 
-  filter(statefp < 60,
-         !statefp %in% c(2, 15)) %>% 
-  rmapshaper::ms_simplify()
+# publics stations only
+state_counts_public <- 
+  ev %>% 
+  filter(access_code == 'public') %>% 
+  count(state, name = "n_public") %>% 
+  as_tibble()
+
+# private stations only
+state_counts_private <- 
+  ev %>% 
+  filter(access_code == 'private') %>% 
+  count(state, name = "n_private") %>% 
+  as_tibble()
+
+# join counts into single df
+state_counts_df <- state_counts_total %>% 
+  left_join(state_counts_public) %>% 
+  left_join(state_counts_private)
+
+saveRDS(state_counts_df, file.path(".","data","state_counts_df") )
+
